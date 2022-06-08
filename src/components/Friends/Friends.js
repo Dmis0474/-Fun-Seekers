cec96465770fcec77b4f6bed32210ac18b22973f
Дмитрий Исаченко <105939958+Dmis0474@users.noreply.github.com>
2022-06-08 21:53:21 +0400

    30th lesson

---
+++ src/components/Friends/Friends.js
@@ -0,0 +1,12 @@
+import React from "react";
+import style from './Musics.module.css'
+
+let Musics = (props) => {
+    return (
+        <div>
+            <h1>Musics в обработке...</h1>
+        </div>
+    )
+}
+
+export default Musics
